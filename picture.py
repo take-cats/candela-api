79c2cb2c09f93e13afa66ce1ce849614399a9e30
DwarfAmil <hjp7126@naver.com>
2023-08-15 17:00:28 +0900

    feat: picture upload

---
+++ picture.py
@@ -0,0 +1,38 @@
+import os
+from fastapi import UploadFile
+from main import app
+
+@app.post("/ai/upload/")
+def get_picture(file: UploadFile):
+    """
+    사진을 받아서 picture/finding 폴더에 넣습니다.
+
+    :param file: 사진을 받아옵니다.
+    """
+
+    UPLOAD_FOLDER = "picture/finding"
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+    
+    return {"message" : "Picture upload success"}
+
+
+@app.post("/ai/data/upload/")
+def get_picture(file: UploadFile):
+    """
+    사진을 받아서 picture/leaning 폴더에 넣습니다.
+    
+    :param file: 사진을 받아옵니다.
+    """
+
+    UPLOAD_FOLDER = "picture/learning"
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+    
+    return {"message" : "Picture upload success"}
