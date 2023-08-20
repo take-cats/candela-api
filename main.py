55925e129e3b55577781895c4d29c71a19bed9fd
DwarfAmil <hjp7126@naver.com>
2023-08-20 22:33:14 +0900

    chore: add `/api`  endpoint  prefix

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 app = FastAPI()
 
+app.include_router(APIRouter(prefix="/api"))
+
+
 @app.get("/")
 def root():
-    return {"status" : 200}
+    return {"status": 200}
+
 
 if __name__ == "__main__":
     os.system("python -m uvicorn main:app")
